dd1df50d6ca052a8d53ca6ccddcfce81c34de992
BryanMtz2483 <mhzb3072@gmail.com>
2025-03-15 15:28:49 -0600

    Actividad 6: Segmentamos las rutas de pokemon que se encontraban en el archivo de index y se creó una carpeta y un archivo para dichas rutas, se agregó el middleware  morgan para tener un registro detallado de las solicitudes http. Agregamos una conexión a una base de datos mysql de pokedex utilizando la función pool para crear múltiples conexiones a la base de datos y se importó la librería util para trabajar de manera asíncrona las peticiones que hagamos a la base de datos mediante las promesas para utilizar valores que pueden estar disponibles en un futuro.

--- config/database.js
+++ config/database.js
@@ -1,7 +1,6 @@
 const mysql = require('mysql'); //importamos mysql 
 const util = require('util'); //importamos la librería util para trabajar de manera asíncrona con las peticiones que hagamos a la base de datos. Una promesa (también conocida como promise) es un objeto que representa un valor que puede estar disponible en el futuro, es decir, un valor que no se conoce inmediatamente, pero se puede esperar que se resuelva en un momento dado.
 
-
 const pool = mysql.createPool({
     connectionLimit: 10,
     host: 'localhost',
